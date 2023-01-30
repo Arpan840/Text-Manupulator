ce7df0d0103df0cd1991dea0aa8e44b440e9f7a7
Arpan Das <arpandas020498@gmail.com>
2023-01-31 00:56:51 +0530

    Updates

---
+++ precache-manifest.89ef90219818db0105657f7e926bdedf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b3237f36ef9adc155ac",
+    "url": "/React-text-util/static/css/main.7eb80412.chunk.css"
+  },
+  {
+    "revision": "5b3237f36ef9adc155ac",
+    "url": "/React-text-util/static/js/main.5b3237f3.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/React-text-util/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "48b12efdd517c360614b",
+    "url": "/React-text-util/static/js/2.48b12efd.chunk.js"
+  },
+  {
+    "revision": "ff85fee9de7e03c5ad65",
+    "url": "/React-text-util/static/js/runtime~main.ff85fee9.js"
+  },
+  {
+    "revision": "be996683f741079a45daf9f61abcbf30",
+    "url": "/React-text-util/index.html"
+  }
+];
